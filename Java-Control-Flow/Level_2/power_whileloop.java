024cd51312bafa4d71fd4ca51793517d09c2abf9
Ram Sharma <ram.sharma_cs.aiml24@gla.ac.in>
2025-08-26 16:10:45 +0530

    power_whileloop.java

---
+++ Java-Control-Flow/Level_2/power_whileloop.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class PowerWhile {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the base number: ");
+        int number = sc.nextInt();
+
+        System.out.print("Enter the power (non-negative integer): ");
+        int power = sc.nextInt();
+
+        if (power < 0) {
+            System.out.println("Power must be a non-negative integer.");
+        } else {
+            int result = 1;
+            int counter = 0;
+
+            while (counter < power) {
+                result *= number;
+                counter++;
+            }
+
+            System.out.println(number + " raised to the power " + power + " is: " + result);
+        }
+
+        sc.close();
+    }
+}
