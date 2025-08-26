8bd978a6cf45278185b01698753f273471a51a00
Ram Sharma <ram.sharma_cs.aiml24@gla.ac.in>
2025-08-26 14:40:56 +0530

    Added countdown.java

---
+++ Java-Control-Flow/Level_1/countdown.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class CountdownWhile {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Input countdown value
+        System.out.print("Enter countdown value: ");
+        int counter = sc.nextInt();
+
+        // While loop countdown
+        while (counter >= 1) {
+            System.out.println(counter);
+            counter--;
+        }
+
+        System.out.println("Rocket Launched!");
+        sc.close();
+    }
+}
