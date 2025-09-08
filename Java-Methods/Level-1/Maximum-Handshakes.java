864ec5ca57bd40e779e205be21aab04982cc70a2
Ram Sharma <ram.sharma_cs.aiml24@gla.ac.in>
2025-09-08 16:53:00 +0530

    Added Maximum-Handshakes.java

---
+++ Java-Methods/Level-1/Maximum-Handshakes.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Handshakes {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter number of students: ");
+        int numberOfStudents = input.nextInt();
+        int handshakes = calculateHandshakes(numberOfStudents);
+        System.out.println("Maximum number of possible handshakes: " + handshakes);
+        input.close();
+    }
+    public static int calculateHandshakes(int n) {
+        return (n * (n - 1)) / 2;
+    }
+}
