5eb258f144858193d13ca904f4c3d181981812d2
Ram Sharma <ram.sharma_cs.aiml24@gla.ac.in>
2025-09-08 17:00:09 +0530

    Added Smallest-and-largest.java

---
+++ Java-Methods/Level-1/Smallest-and-largest.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class SmallestLargest {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter number 1: ");
+        int num1 = input.nextInt();
+        System.out.print("Enter number 2: ");
+        int num2 = input.nextInt();
+        System.out.print("Enter number 3: ");
+        int num3 = input.nextInt();
+        int[] result = findSmallestAndLargest(num1, num2, num3);
+        System.out.println("Smallest: " + result[0] + ", Largest: " + result[1]);
+        input.close();
+    }
+    public static int[] findSmallestAndLargest(int number1, int number2, int number3) {
+        int smallest = number1;
+        int largest = number1;
+        if (number2 < smallest) smallest = number2;
+        if (number3 < smallest) smallest = number3;
+        if (number2 > largest) largest = number2;
+        if (number3 > largest) largest = number3;
+        return new int[]{smallest, largest};
+    }
+}
